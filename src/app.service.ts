e334292641ea3e607697be5536732649847e2810
samuelcsouza <samuel_dacruz@hotmail.com>
2026-05-18 21:24:13 -0300

    feat: healthcheck route

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  healthcheck(): { status: 'ok', uptime: number } {
+    return { status: 'ok', uptime: process.uptime() };
+  }
 }
